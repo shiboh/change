2c17bc779029c079cf7d13d7e31e2d5d98e7689f
shang1jk <shang1jk@gmail.com>
2015-01-29 20:26:47 +0800

    改字体

--- app/src/main/java/com/chnMicro/MFExchange/activity/MainActivity.java
+++ app/src/main/java/com/chnMicro/MFExchange/activity/MainActivity.java
@@ -1,39 +1,37 @@
 package com.chnMicro.MFExchange.activity;
 
-import android.view.View;
-import android.widget.Button;
+import android.graphics.Typeface;
+import android.widget.TextView;
 
 import com.chnMicro.MFExchange.R;
 
 import butterknife.InjectView;
-import butterknife.OnClick;
 
 /**
  * Created by Enel on 2015/1/26.
  */
 public class MainActivity extends BaseActivity {
-    @InjectView(R.id.btn) Button btn;
+    @InjectView(R.id.tv_topbar_left) TextView tvTopbarLeft;
+    @InjectView(R.id.tv_topbar_medium) TextView tvTopbarMedium;
+    @InjectView(R.id.tv_topbar_right) TextView tvTopbarRight;
 
     @Override protected void prepare() {
 
     }
 
     @Override protected void setContentView() {
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_main);
     }
 
     @Override protected void initViews() {
-        btn.setText("this is B");
+        Typeface tf
+        tvTopbarLeft.setTypeface(tf);
+
+
+
     }
 
     @Override protected void initData() {
 
     }
-
-    @OnClick(R.id.btn)
-    public void go2C(View v) {
-//        Intent intent = new Intent(this, CActivity.class);
-//        startActivity(intent);
-    }
-
 }
